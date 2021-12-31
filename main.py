890cab91d4ec5e96e0349bebbd376b2897a83a77
BrHCastro <hendecastro@gmail.com>
2021-12-30 21:02:18 -0300

    Atribuindo valores para as variáveis pelo input

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-largura = 5
-altura = 2.9
-profundidade = 3
+largura: float = float(input("Qual a largura do cômodo? "))
+profundidade: float = float(input("Qual a profundidade do cômodo? "))
+altura: float = 2.9
 
-print(2 * (largura + profundidade) * altura)
-print(largura * profundidade)
+print("A area das paredes é: ", (2 * (largura + profundidade) * altura))
