9ad30e45aa167d96645af70a94c0f232fbe04871
BrHCastro <hendecastro@gmail.com>
2021-12-30 21:43:33 -0300

    Criação dos métodos

--- main.py
+++ main.py
@@ -6,13 +6,19 @@ largura: float = float(input("Qual a largura do cômodo? "))
 profundidade: float = float(input("Qual a profundidade do cômodo? "))
 altura: float = 2.9
 
-calc.area_paredes: float = 2 * (largura + profundidade) * altura
-calc.area_teto: float = largura * profundidade
+# calc.area_paredes: float = 2 * (largura + profundidade) * altura
+
+# calc.area_teto: float = largura * profundidade
 
 print("A area das paredes é: ",
-        calc.area_paredes
+        calc.calcular_area_parede(largura, profundidade, altura)
       )
+
+print("A area do této é: ",
+        calc.calcular_area_teto(largura, profundidade)
+      )
+
 print("A litragem de tinta necessária é: ",
-        (calc.area_paredes + calc.area_teto) / 10  # 10 é o rendimento da tinta. 10m² / lt
+        calc.calcular_litragem_necessaria()
       )
 
