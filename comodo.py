8793e6dfeff20d284d3351799b5e3143ebe18215
BrHCastro <hendecastro@gmail.com>
2021-12-30 22:31:35 -0300

    Propriedades e Exceções

--- comodo.py
+++ comodo.py
@@ -1,9 +1,37 @@
 class Comodo:
-    largura: float
-    profundidade: float
-    altura: float
+    __largura: float
+    __profundidade: float
+    __altura: float
 
     def __init__(self, largura, profundidade):  # Metodo construtor da classe (Será executado quando a classe for instânciada.
-        self.largura = float(largura)
-        self.profundidade = float(profundidade)
-        self.altura = 2.9
+        self.largura = largura
+        self.profundidade = profundidade
+        self.__altura = 2.9
+
+    @property  # Indica que esse método é uma propriedade
+    def largura(self):
+        return self.__largura
+
+    @property
+    def profundidade(self):
+        return self.__profundidade
+
+    @property
+    def altura(self):
+        return self.__altura
+
+    @largura.setter
+    def largura(self, largura):
+        try:
+            self.__largura = float(largura)
+        except Exception:
+            print("O valor informado da largura é inválida!")
+            exit()
+
+    @profundidade.setter
+    def profundidade(self, profundidade):
+        try:
+            self.__profundidade = float(profundidade)
+        except Exception:
+            print("O valor informado da profundidade é inválida!")
+            exit()
