1768dc738d1f53cb690172b2bc05cb18eb677d70
BrHCastro <hendecastro@gmail.com>
2021-12-30 21:10:43 -0300

    Resolvendo calculo da area das paredes e této e a quantidade de tinta

--- main.py
+++ main.py
@@ -2,4 +2,13 @@ largura: float = float(input("Qual a largura do cômodo? "))
 profundidade: float = float(input("Qual a profundidade do cômodo? "))
 altura: float = 2.9
 
-print("A area das paredes é: ", (2 * (largura + profundidade) * altura))
+area_paredes: float = 2 * (largura + profundidade) * altura
+area_teto: float = largura * profundidade
+
+print("A area das paredes é: ",
+        area_paredes
+      )
+print("A litragem de tinta necessária é: ",
+        (area_paredes + area_teto) / 10  # 10 é o rendimento da tinta. 10m² / lt
+      )
+
